76aea4f2597f1e5fe3055eef0c5ff900c011d978
Christos <ch.panagiotou@esdalab.ece.uop.gr>
2022-06-15 00:20:06 +0300

    updates for 2022 wip

---
+++ 2021/README.md
@@ -0,0 +1,34 @@
+# Getting Started
+
+### General
+This project is to support the Course MSC-101. It is supposed to be used as the basis for the courses's assignment.
+The details of the assignment are described here (in greek):
+
+## Local Development environment
+
+### Prerequisites
+- JDK 8
+- Maven3
+- An MQTT broker (optional): https://www.emqx.io/
+- An MQTT client application for testing (optional): https://chrome.google.com/webstore/detail/mqttbox/kaajoficamnjijhkeomgfljpicifbkaf
+
+### Configure
+Before running the application you need to setup the MQTT broker connection details.
+
+You will find them in `src/main/resources/application.yml`
+
+
+### Test the application
+Run Tests:
+
+`mvn test`
+
+### Run the application
+Execute the following from command line:
+
+`mvn spring-boot:run`
+
+## Preconfigured Development Environment
+To skip all the above and have a development environment ready sooner, you can import the provided ova to your favorite
+hypervisor (link has been shared internally through the assignment description).
+
---
+++ 2022/mbed/mqtt_subscriber.c
@@ -0,0 +1,105 @@
+#define USE_LCD 0
+ 
+#if USE_LCD
+#include "C12832.h"
+ 
+// the actual pins are defined in mbed_app.json and can be overridden per target
+C12832 lcd(LCD_MOSI, LCD_SCK, LCD_MISO, LCD_A0, LCD_NCS);
+ 
+#define logMessage lcd.cls();lcd.printf
+ 
+#else
+ 
+#define logMessage printf
+ 
+#endif
+ 
+#define MQTTCLIENT_QOS2 1
+ 
+#include "easy-connect.h"
+#include "MQTTNetwork.h"
+#include "MQTTmbed.h"
+#include "MQTTClient.h"
+ 
+int arrivedcount = 0;
+ 
+ 
+void messageArrived(MQTT::MessageData& md)
+{
+    MQTT::Message &message = md.message;
+    logMessage("Message arrived: qos %d, retained %d, dup %d, packetid %d\r\n", message.qos, message.retained, message.dup, message.id);
+    logMessage("Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
+    ++arrivedcount;
+}
+ 
+ 
+int main(int argc, char* argv[])
+{
+    // MQTT broker hostname
+    const char* HOST = "dock1.pikei.io";
+    // MQTT broker port
+    const int PORT = 1884;
+
+    float sample_sensor_value = 28.7;
+
+    // MQTT topics for temperature & humidity
+    const char* TEMPERATURE_TOPIC = "/uop/msc/hmmy101/iot/temperature";
+    const char* HUMIDITY_TOPIC = "/uop/msc/hmmy101/iot/humidity";
+ 
+    logMessage("HelloMQTT: version is %.2f\r\n", version);
+ 
+    NetworkInterface* network = easy_connect(true);
+    if (!network) {
+        return -1;
+    }
+ 
+    MQTTNetwork mqttNetwork(network);
+ 
+    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
+ 
+    logMessage("Connecting to %s:%d\r\n", HOST, PORT);
+    int rc = mqttNetwork.connect(HOST, PORT);
+    if (rc != 0)
+        logMessage("rc from TCP connect is %d\r\n", rc);
+ 
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.MQTTVersion = 3;
+
+    // MQTT unique client id. Update this
+    data.clientID.cstring = "tmp";
+    // MQTT user credentials. Update this
+    data.username.cstring = "testuser";
+    data.password.cstring = "testpassword";
+
+    if ((rc = client.connect(data)) != 0)
+        logMessage("rc from MQTT connect is %d\r\n", rc);
+ 
+    if ((rc = client.subscribe(TEMPERATURE_TOPIC, MQTT::QOS2, messageArrived)) != 0)
+        logMessage("rc from MQTT subscribe is %d\r\n", rc);
+ 
+    MQTT::Message message;
+ 
+    char buf[100];
+    sprintf(buf, "%f", sample_sensor_value);
+    
+    message.qos = MQTT::QOS0;
+    message.retained = false;
+    message.dup = false;
+    message.payload = (void*)buf;
+    message.payloadlen = strlen(buf)+1;
+    rc = client.publish(TEMPERATURE_TOPIC, message);
+    while (arrivedcount < 1)
+        client.yield(100);
+ 
+    if ((rc = client.unsubscribe(TEMPERATURE_TOPIC)) != 0)
+        logMessage("rc from unsubscribe was %d\r\n", rc);
+ 
+    if ((rc = client.disconnect()) != 0)
+        logMessage("rc from disconnect was %d\r\n", rc);
+ 
+    mqttNetwork.disconnect();
+ 
+    logMessage("Version %.2f: finish %d msgs\r\n", version, arrivedcount);
+ 
+    return 0;
+}
